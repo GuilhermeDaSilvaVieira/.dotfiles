d6fa61172a826e196f0d411f1be3ed1aecd09e7d
GuilhermeDaSilvaVieira <guilhermedas.vieira@gmail.com>
2022-09-19 19:03:43 -0300

    add: fish

---
+++ fish/config.fish
@@ -0,0 +1,14 @@
+﻿if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+export FZF_DEFAULT_COMMAND="fd -H"
+export BROWSER="/usr/bin/librewolf"
+fish_add_path "/home/guilherme/Dev/Flutter/SDK/flutter/bin/"
+fish_add_path "/home/guilherme/.cargo/bin/"
+
+alias l "exa -la --sort=type"
+alias v "nvim"
+alias cat "bat"
+
+set fish_greeting
