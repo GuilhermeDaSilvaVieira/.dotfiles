93a251db9b2e73063328cb85f5d5cf50c6d3f8c1
Guilherme Da Silva Vieira <guilhermedas.vieira@gmail.com>
2022-12-10 11:08:25 -0300

    include tile.left

--- awesome/config/vars.lua
+++ awesome/config/vars.lua
@@ -5,7 +5,7 @@ local awful = require("awful")
 _M.layouts = {
   --[[ awful.layout.suit.floating, ]]
   awful.layout.suit.tile,
-  --[[ awful.layout.suit.tile.left, ]]
+  awful.layout.suit.tile.left,
   --[[ awful.layout.suit.tile.bottom, ]]
   awful.layout.suit.tile.top,
   --[[ awful.layout.suit.fair, ]]
