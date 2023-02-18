4de0c6a4d223f8df76c12ac4f2ffe2912d5a4152
Guilherme Da Silva Vieira <guilhermedas.vieira@gmail.com>
2023-02-18 20:31:15 -0300

    remove spell by default, no swap files!, remove redunduncy

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -12,9 +12,6 @@ vim.o.mouse = "a"
 -- Enable break indent
 vim.o.breakindent = true
 
--- Save undo history
-vim.o.undofile = true
-
 -- Case insensitive searching UNLESS /C or capital in search
 vim.o.ignorecase = true
 vim.o.smartcase = true
@@ -51,16 +48,16 @@ vim.o.scrolloff = 8
 -- Visual reminder of good text width
 vim.o.colorcolumn = "80"
 
--- Persistent undo
+-- NO SWAP FILES!!!!
+vim.o.swapfile = false
+
+-- Save undo history
 vim.o.undofile = true
 vim.o.undodir = os.getenv("HOME") .. "/.nvim/undo/"
 
 -- Security backup
 vim.o.backup = true
 vim.o.backupdir = os.getenv("HOME") .. "/.nvim/backup/"
-
--- Check grammar
-vim.o.spell = true
 
 -- Use real colors in themes
 vim.o.termguicolors = true
