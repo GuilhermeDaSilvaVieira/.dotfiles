09e14a5b6d1c7738680b84e16fb86e9e36ecba11
Guilherme Da Silva Vieira <guilhermedas.vieira@gmail.com>
2023-02-19 12:54:58 -0300

    last release is way too old

--- nvim/lua/plugins/cmp.lua
+++ nvim/lua/plugins/cmp.lua
@@ -2,6 +2,7 @@
 return {
   "hrsh7th/nvim-cmp",
   event = "InsertEnter",
+  vesion = false, -- last release is way too old
   dependencies = {
     "hrsh7th/cmp-nvim-lsp",
     "hrsh7th/cmp-buffer",
