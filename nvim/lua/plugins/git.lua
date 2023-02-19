7df411de198ccf8d11b9484024b475aeec4e7161
Guilherme Da Silva Vieira <guilhermedas.vieira@gmail.com>
2023-02-19 13:15:01 -0300

    lazy loading and formatting

--- nvim/lua/plugins/git.lua
+++ nvim/lua/plugins/git.lua
@@ -1,17 +1,18 @@
 -- Git related plugins
 return {
-    "lewis6991/gitsigns.nvim",
-    config = function()
-      -- Gitsigns
-      -- See `:help gitsigns.txt`
-      require("gitsigns").setup({
-        signs = {
-          add = { text = "+" },
-          change = { text = "~" },
-          delete = { text = "_" },
-          topdelete = { text = "‾" },
-          changedelete = { text = "~" },
-        },
-      })
-    end,
+  "lewis6991/gitsigns.nvim",
+  event = { "BufReadPre", "BufNewFile" },
+  config = function()
+    -- Gitsigns
+    -- See `:help gitsigns.txt`
+    require("gitsigns").setup({
+      signs = {
+        add = { text = "+" },
+        change = { text = "~" },
+        delete = { text = "_" },
+        topdelete = { text = "‾" },
+        changedelete = { text = "~" },
+      },
+    })
+  end,
 }
--- nvim/lua/plugins/indent.lua
+++ nvim/lua/plugins/indent.lua
@@ -1,9 +1,12 @@
-  -- Add indentation guides even on blank lines
-  return {"lukas-reineke/indent-blankline.nvim", config= function ()
--- See `:help indent_blankline.txt`
-require("indent_blankline").setup({
-    char = "┊",
-    show_trailing_blankline_indent = false,
-})
-    
-  end}
+-- Add indentation guides even on blank lines
+return {
+  "lukas-reineke/indent-blankline.nvim",
+  event = "VeryLazy",
+  config = function()
+    -- See `:help indent_blankline.txt`
+    require("indent_blankline").setup({
+      char = "┊",
+      show_trailing_blankline_indent = false,
+    })
+  end,
+}
