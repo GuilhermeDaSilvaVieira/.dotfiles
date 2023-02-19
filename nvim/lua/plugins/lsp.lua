2e4deb4f6ec2cff25f96633aab4cdc09575b62e9
Guilherme Da Silva Vieira <guilhermedas.vieira@gmail.com>
2023-02-19 12:22:10 -0300

    remove useless comments

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -12,26 +12,16 @@ return {
     "folke/neodev.nvim",
   },
   config = function()
-    -- LSP settings.
-    --  This function gets run when an LSP connects to a particular buffer.
     local on_attach = function(_, bufnr)
-      -- NOTE: Remember that lua is a real programming language, and as such it is possible
-      -- to define small helper and utility functions so you don't have to repeat yourself
-      -- many times.
-      --
-      -- In this case, we create a function that lets us more easily define mappings specific
-      -- for LSP related items. It sets the mode, buffer and description for us each time.
       local nmap = function(keys, func, desc)
         if desc then
           desc = "LSP: " .. desc
         end
-
         vim.keymap.set("n", keys, func, { buffer = bufnr, desc = desc })
       end
 
       nmap("<leader>rn", vim.lsp.buf.rename, "[R]e[n]ame")
       nmap("<leader>ca", vim.lsp.buf.code_action, "[C]ode [A]ction")
-
       nmap("gd", vim.lsp.buf.definition, "[G]oto [D]efinition")
       nmap(
         "gr",
@@ -45,47 +35,19 @@ return {
         require("telescope.builtin").lsp_document_symbols,
         "[D]ocument [S]ymbols"
       )
-
-      -- See `:help K` for why this keymap
       nmap("K", vim.lsp.buf.hover, "Hover Documentation")
-      -- nmap("<C-k>", vim.lsp.buf.signature_help, "Signature Documentation")
-
-      -- Lesser used LSP functionality
       nmap("gD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
-
-      -- Diagnostic keymaps
       nmap("[d", vim.diagnostic.goto_prev, "Goto Previous Diagnostic")
       nmap("]d", vim.diagnostic.goto_next, "Goto Next Diagnostic")
       nmap("gl", vim.diagnostic.open_float, "Open Floating Diagnostic")
-
       -- Create a command `:Format` local to the LSP buffer
       vim.api.nvim_buf_create_user_command(bufnr, "Format", function(_)
         vim.lsp.buf.format()
       end, { desc = "Format current buffer with LSP" })
     end
-
-    -- Enable the following language servers
-    --  Feel free to add/remove any LSPs that you want here. They will automatically be installed.
-    --
-    --  Add any additional override configuration in the following tables. They will be passed to
-    --  the `settings` field of the server config. You must look up that documentation yourself.
-    local servers = {
-      rust_analyzer = {},
-      lua_ls = {
-        Lua = {
-          workspace = { checkThirdParty = false },
-          telemetry = { enable = false },
-          format = { enable = false },
-          settings = {
-            completion = { callSnippet = "Replace" },
-          },
-        },
-      },
-    }
-
     -- Setup neovim lua configuration
     require("neodev").setup()
-    --
+
     -- nvim-cmp supports additional completion capabilities, so broadcast that to servers
     local capabilities = vim.lsp.protocol.make_client_capabilities()
     capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
@@ -93,12 +55,22 @@ return {
     -- Setup mason so it can manage external tooling
     require("mason").setup()
 
+    local servers = {
+      rust_analyzer = {},
+      lua_ls = {
+        Lua = {
+          workspace = { checkThirdParty = false },
+          telemetry = { enable = false },
+          format = { enable = false },
+          settings = { completion = { callSnippet = "Replace" } },
+        },
+      },
+    }
+
     -- Ensure the servers above are installed
     local mason_lspconfig = require("mason-lspconfig")
 
-    mason_lspconfig.setup({
-      ensure_installed = vim.tbl_keys(servers),
-    })
+    mason_lspconfig.setup({ ensure_installed = vim.tbl_keys(servers) })
 
     mason_lspconfig.setup_handlers({
       function(server_name)
