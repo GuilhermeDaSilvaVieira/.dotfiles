6c15587174fd67a3860cd1a8b8f3e127145d5daf
Guilherme Da Silva Vieira <guilhermedas.vieira@gmail.com>
2023-02-19 12:20:22 -0300

    format file

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -11,7 +11,7 @@ return {
   -- Theme inspired by Atom
   {
     "navarasu/onedark.nvim",
-    lazy =true,
+    lazy = true,
     -- priority = 1000,
     config = function()
       -- vim.cmd.colorscheme("onedark")
