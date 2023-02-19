06a5e25a7f1bb0994a3cd29290c98e5ee967ced1
Guilherme Da Silva Vieira <guilhermedas.vieira@gmail.com>
2023-02-19 12:24:40 -0300

    add treesitter config

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,13 +1,34 @@
 -- Highlight, edit, and navigate code
 return {
   "nvim-treesitter/nvim-treesitter",
-  build = function()
-    pcall(require("nvim-treesitter.install").update({ with_sync = true }))
-  end,
   dependencies = {
     -- Additional text objects via treesitter
     "nvim-treesitter/nvim-treesitter-textobjects",
     -- Bracket Colorizer
     "p00f/nvim-ts-rainbow",
   },
+  config = function()
+    require("nvim-treesitter.install").update({ with_sync = true })
+
+    require("nvim-treesitter.configs").setup({
+      ensure_installed = "all",
+      sync_install = false,
+      highlight = {
+        enable = true,
+        additional_vim_regex_highlighting = false,
+      },
+      indent = {
+        enable = true,
+      },
+      rainbow = {
+        enable = true,
+        extended_mode = true,
+        max_file_lines = nil,
+      },
+      context_commentstring = {
+        enable = true,
+        enable_autocmd = false,
+      },
+    })
+  end,
 }
