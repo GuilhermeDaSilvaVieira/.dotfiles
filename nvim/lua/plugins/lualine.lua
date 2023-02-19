b11c17f0fd2fb93fc5e28ed36c6ac1f39416b605
Guilherme Da Silva Vieira <guilhermedas.vieira@gmail.com>
2023-02-19 12:22:53 -0300

    lazyvim lualine

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -1,16 +1,93 @@
+-- Status line
 return {
-    'nvim-lualine/lualine.nvim',
+  "nvim-lualine/lualine.nvim",
+  event = "VeryLazy",
   dependencies = "nvim-tree/nvim-web-devicons",
-    config = function()
-        -- Set lualine as statusline
-        -- See `:help lualine.txt`
-        require("lualine").setup({
-            options = {
-                icons_enabled = true,
-                theme = "auto",
-                component_separators = "|",
-                section_separators = "",
-            },
-        })
-    end,
+  opts = function(plugin)
+    local function fg(name)
+      return function()
+        ---@type {foreground?:number}?
+        local hl = vim.api.nvim_get_hl_by_name(name, true)
+        return hl
+          and hl.foreground
+          and { fg = string.format("#%06x", hl.foreground) }
+      end
+    end
+
+    return {
+      options = {
+        theme = "auto",
+        globalstatus = true,
+        disabled_filetypes = { statusline = { "dashboard", "lazy", "alpha" } },
+      },
+      sections = {
+        lualine_a = { "mode" },
+        lualine_b = { "branch" },
+        lualine_c = {
+          {
+            "diagnostics",
+          },
+          {
+            "filetype",
+            icon_only = true,
+            separator = "",
+            padding = { left = 1, right = 0 },
+          },
+          {
+            "filename",
+            path = 1,
+          },
+          {
+            function()
+              return require("nvim-navic").get_location()
+            end,
+            cond = function()
+              return package.loaded["nvim-navic"]
+                and require("nvim-navic").is_available()
+            end,
+          },
+        },
+        lualine_x = {
+          {
+            function()
+              return require("noice").api.status.command.get()
+            end,
+            cond = function()
+              return package.loaded["noice"]
+                and require("noice").api.status.command.has()
+            end,
+            color = fg("Statement"),
+          },
+          {
+            function()
+              return require("noice").api.status.mode.get()
+            end,
+            cond = function()
+              return package.loaded["noice"]
+                and require("noice").api.status.mode.has()
+            end,
+            color = fg("Constant"),
+          },
+          {
+            require("lazy.status").updates,
+            cond = require("lazy.status").has_updates,
+            color = fg("Special"),
+          },
+          {
+            "diff",
+          },
+        },
+        lualine_y = {
+          { "progress", separator = " ", padding = { left = 1, right = 0 } },
+          { "location", padding = { left = 0, right = 1 } },
+        },
+        lualine_z = {
+          function()
+            return " " .. os.date("%R")
+          end,
+        },
+      },
+      extensions = { "neo-tree" },
+    }
+  end,
 }
