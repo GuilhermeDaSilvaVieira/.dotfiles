8ba2b94385b8fb37cc196319f6b7a7073e9c5f69
Guilherme Da Silva Vieira <guilhermedas.vieira@gmail.com>
2023-02-19 08:03:33 -0300

    lazy load snippets

--- nvim/lua/plugins/cmp.lua
+++ nvim/lua/plugins/cmp.lua
@@ -19,6 +19,8 @@ return {
     local cmp = require("cmp")
     local luasnip = require("luasnip")
 
+    require("luasnip.loaders.from_vscode").lazy_load()
+
     cmp.setup({
       snippet = {
         expand = function(args)
