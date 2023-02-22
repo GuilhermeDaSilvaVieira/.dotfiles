9709cda6a87654fcaff80adeda524abb194bc9c8
Guilherme Da Silva Vieira <guilhermedas.vieira@gmail.com>
2023-02-22 10:20:11 -0300

    installing only needed parsers

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -12,7 +12,7 @@ return {
     require("nvim-treesitter.install").update({ with_sync = true })
 
     require("nvim-treesitter.configs").setup({
-      ensure_installed = "all",
+      ensure_installed = { "rust", "lua", "vim", "help" },
       sync_install = false,
       highlight = {
         enable = true,
