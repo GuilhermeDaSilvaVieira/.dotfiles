e29dd75d2ee1d0173751a63625e1027f31c84c7d
Guilherme Da Silva Vieira <guilhermedas.vieira@gmail.com>
2023-04-01 18:44:45 -0300

    add: calendar popup to clock

--- awesome/bar/modules/clock/init.lua
+++ awesome/bar/modules/clock/init.lua
@@ -1,7 +1,11 @@
 local wibox = require("wibox")
+local awful = require("awful")
 
 local clock = {}
 
 clock.clock = wibox.widget.textclock("%a %F %T", 1)
+
+local calendar = awful.widget.calendar_popup.month()
+calendar:attach(clock.clock, "tc")
 
 return clock
