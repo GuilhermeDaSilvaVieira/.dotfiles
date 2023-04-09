3b653745bdd3b4e8f65be7d9c4311814730e21c1
Guilherme Da Silva Vieira <guilhermedas.vieira@gmail.com>
2023-04-09 09:44:34 -0300

    fix: lsp highlight overwritting treesitter

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -21,6 +21,8 @@ return {
     end
 
     local on_attach = function(client, bufnr)
+       client.server_capabilities.semanticTokensProvider = nil
+
       attach_navic(client, bufnr)
 
       local signs =
