59e0e215d0e385e730c910a683399cafa48982ae
Guilherme Da Silva Vieira <guilhermedas.vieira@gmail.com>
2023-04-09 09:41:16 -0300

    fix: help is no longer provided

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -12,7 +12,7 @@ return {
     require("nvim-treesitter.install").update({ with_sync = true })
 
     require("nvim-treesitter.configs").setup({
-      ensure_installed = { "rust", "lua", "vim", "help" },
+      ensure_installed = { "rust", "lua", "vim" },
       sync_install = false,
       highlight = {
         enable = true,
