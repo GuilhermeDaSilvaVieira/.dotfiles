98c4e80847d1667a11fb4fb12c674ec2a2f0e07b
Guilherme Da Silva Vieira <guilhermedas.vieira@gmail.com>
2023-04-14 12:25:59 -0300

    remove spaces in between

--- awesome/bar/modules/username/init.lua
+++ awesome/bar/modules/username/init.lua
@@ -5,7 +5,7 @@ local wibox = require("wibox")
 
 username.username = wibox.widget.textbox()
 awful.spawn.easy_async("whoami", function(output)
-  username.username.text = string.format(" %s ", output)
+  username.username.text = string.format("%s", output)
 end)
 
 return username
