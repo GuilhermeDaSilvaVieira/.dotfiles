16eb03c8b7aa5ff5972906272030fa4cf070728d
Guilherme Da Silva Vieira <guilhermedas.vieira@gmail.com>
2025-04-23 12:08:00 -0300

    fix: remove nixos related cmd

--- fish/config.fish
+++ fish/config.fish
@@ -15,52 +15,10 @@ end
 
 function h
     if test -n "$argv"
-        hx $argv
+        helix $argv
     else
-        hx (fzf)
+        helix (fzf)
     end
-end
-
-function nsync
-    set initial_dir $PWD
-
-    # Avoids multiple arguments
-    if test (count $argv) -gt 1
-        echo "Too many arguments"
-        return
-    end
-
-    # Check if the first argument is '-s'
-    if test (count $argv) -gt 0
-        if test "$argv[1]" = -s
-            echo "Updating..."
-            cd $HOME/nixos_config/
-            nix flake update
-        else
-            echo "Invalid argument"
-            echo "Moving back to initial directory..."
-            cd $initial_dir
-            return
-        end
-    end
-
-    echo "Copying..."
-    cp -r $HOME/nixos_config/ $HOME/nixos_config_without_git
-
-    echo "Changing to copy directory..."
-    cd $HOME/nixos_config_without_git || return # Exit if cd fails
-
-    echo "Removing .git"
-    rm -rf .git/
-
-    echo "Rebuilding..."
-    doas nixos-rebuild switch --flake .
-
-    echo "Removing copy..."
-    rm -rf $HOME/nixos_config_without_git/
-
-    echo "Moving back to initial directory..."
-    cd $initial_dir
 end
 
 fish_add_path $HOME/.cargo/bin/
@@ -81,11 +39,3 @@ starship init fish | source
 enable_transience
 
 zoxide init fish | source
-
-if test -f "$HOME/.dotfiles/fish/lf_icons.fish"
-    source $HOME/.dotfiles/fish/lf_icons.fish
-end
-
-if test -f "$HOME/nixos_config/.dotfiles/fish/lf_icons.fish"
-    source $HOME/nixos_config/.dotfiles/fish/lf_icons.fish
-end
