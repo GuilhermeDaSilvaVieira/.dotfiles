7a43e97dc21df64f1ce6f3b4b7e07041626f9388
Guilherme Da Silva Vieira <guilhermedas.vieira@gmail.com>
2025-07-31 00:26:06 -0300

    remove nvidia related environment variables

--- fish/config.fish
+++ fish/config.fish
@@ -26,17 +26,17 @@ end
 set -gx EDITOR helix
 set -gx VISUAL helix
 set -gx FZF_DEFAULT_COMMAND "fd -H"
-set -gx LIBSEAT_BACKEND logind
-set -gx WLR_NO_HARDWARE_CURSORS = 1
-set -gx WLR_RENDERER vulkan
-set -gx QT_QPA_PLATFORM = "wayland;xcb"
-set -gx KITTY_ENABLE_WAYLAND = 1
-set -gx LIBVA_DRIVER_NAME = nvidia
-set -gx XDG_SESSION_TYPE = wayland
-set -gx GBM_BACKEND = nvidia-drm
-set -gx __GLX_VENDOR_LIBRARY_NAME = nvidia
+# set -gx LIBSEAT_BACKEND logind
+# set -gx WLR_NO_HARDWARE_CURSORS = 1
+# set -gx WLR_RENDERER vulkan
+# set -gx QT_QPA_PLATFORM = "wayland;xcb"
+# set -gx KITTY_ENABLE_WAYLAND = 1
+# set -gx LIBVA_DRIVER_NAME = nvidia
+# set -gx XDG_SESSION_TYPE = wayland
+# set -gx GBM_BACKEND = nvidia-drm
+# set -gx __GLX_VENDOR_LIBRARY_NAME = nvidia
 # set -gx ELECTRON_OZONE_PLATFORM_HINT = auto
-set -gx NVD_BACKEND direct
+# set -gx NVD_BACKEND direct
 
 set fish_greeting
 
