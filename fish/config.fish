7c026b828a8ebf8519e15543f2521cde5bd89688
Guilherme Da Silva Vieira <guilhermedas.vieira@gmail.com>
2025-08-05 11:55:59 -0300

    nvidia wayland

--- fish/config.fish
+++ fish/config.fish
@@ -20,24 +20,26 @@ fish_add_path $HOME/.pub-cache/bin/
 if test "$USER" = franky
     set -gx ANDROID_HOME $HOME/Android/Sdk/
     set -gx CHROME_EXECUTABLE /usr/bin/chromium
-    mise activate fish | source
+    # mise activate fish | source
 end
 
 set -gx EDITOR helix
 set -gx VISUAL helix
 set -gx FZF_DEFAULT_COMMAND "fd -H"
-set -gx NOUVEAU_USE_ZINK 1
+# set -gx NOUVEAU_USE_ZINK 1
 # set -gx LIBSEAT_BACKEND logind
+# set -gx DISPLAY ":1"
 # set -gx WLR_NO_HARDWARE_CURSORS = 1
 # set -gx WLR_RENDERER vulkan
-# set -gx QT_QPA_PLATFORM = "wayland;xcb"
-# set -gx KITTY_ENABLE_WAYLAND = 1
-# set -gx LIBVA_DRIVER_NAME = nvidia
-# set -gx XDG_SESSION_TYPE = wayland
-# set -gx GBM_BACKEND = nvidia-drm
-# set -gx __GLX_VENDOR_LIBRARY_NAME = nvidia
-# set -gx ELECTRON_OZONE_PLATFORM_HINT = auto
-# set -gx NVD_BACKEND direct
+set -gx QT_QPA_PLATFORM "wayland;xcb"
+set -gx KITTY_ENABLE_WAYLAND 1
+set -gx LIBVA_DRIVER_NAME nvidia
+set -gx XDG_SESSION_TYPE wayland
+set -gx GBM_BACKEND nvidia-drm
+set -gx GDK_BACKEND "wayland,x11"
+set -gx __GLX_VENDOR_LIBRARY_NAME nvidia
+set -gx ELECTRON_OZONE_PLATFORM_HINT auto
+set -gx NVD_BACKEND direct
 
 set fish_greeting
 
