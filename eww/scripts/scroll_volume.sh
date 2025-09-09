bc5f290e2e4fd3713424fca92136e44403ab0661
Guilherme Da Silva Vieira <guilhermedas.vieira@gmail.com>
2025-09-09 10:42:09 -0300

    make bar consistent with river volume shortcut

--- eww/scripts/scroll_volume.sh
+++ eww/scripts/scroll_volume.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 if [[ $1 == "up" ]]; then
-	wpctl set-volume @DEFAULT_SINK@ 2%+ -l 1
+	wpctl set-volume @DEFAULT_SINK@ 5%+ -l 1
 else
-	wpctl set-volume @DEFAULT_SINK@ 2%- -l 1
+	wpctl set-volume @DEFAULT_SINK@ 5%- -l 1
 fi
